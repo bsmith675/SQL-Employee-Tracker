ce335ad672364f839b21c7eb34389dadd1ab304d
Ben Smith <benlsmith329@gmail.com>
2022-05-24 20:10:58 -0400

    Final Push

---
+++ db/seed.sql
@@ -0,0 +1,13 @@
+use employees;
+
+INSERT INTO department
+    (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    
